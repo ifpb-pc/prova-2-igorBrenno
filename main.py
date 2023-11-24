a03e7d1e5b504382af5815d672a277269fd4c3fd
Igor Brenno Dantas <54316405+igorBrenno@users.noreply.github.com>
2023-11-24 20:17:42 +0000

    feat: questao 1 e 2

--- main.py
+++ main.py
@@ -13,29 +13,48 @@ def ler_valores():
 def formatar_alturas(lista):
     pass
 
-def intercalar_listas(lista1, lista2):
-    pass
-
-def lista_maior18(pessoas):
-    pass
-
-def q1(pessoas = {"Joao": 25, "Maria": 10}):
-    # resultado = lista_maior18(pessoas)
-    # return resultado
+def q1(pessoas = {"Joao": 25, "Maria": 20, "Ana": 30}):
+    # lista_oredenada = sorted(pessoas, key=lambda dicionario: dicionario)
+    lista = []
+    dic = pessoas
+    for i in sorted(pessoas, key = lambda dicionario : dicionario):
+        if (dic[i] >= 18):
+            lista.append(i)
+    return(lista)
 
 def q2(lista1 = [1, 3, 5], lista2 = [2, 4, 6, 8, 10]):
-    # resultado_intercalado = intercalar_listas(lista1, lista2)
-    # return resultado_intercalado
+    lista = []
+    maior = 0
+    if len(lista1) > len(lista2): 
+        maior = len(lista1)
+    else:
+        maior = len(lista2)
+
+    for i in range(maior):
+        if i < len(lista1):
+            lista.append(lista1[i])
+        if i < len(lista2):
+            lista.append(lista2[i])
+    return(lista)
 
 def q3(valores = None):
     #valores = ler_valores()
     # pares, impares = processa_lista(valores)
     # return pares, impares
+    valor = []
+    while True:
+        inp = input()
+        if (inp == 0):
+            break
+        else:
+            valor.append(inp)
+    print(valor)
 
 def q4(valores = None):
     #valores = ler_valores()
     # lista_ambrosina = organizar_alturas(valores)
     # return formatar_alturas(lista_ambrosina)
+    pass
 
 
 
@@ -44,6 +63,7 @@ def q4(valores = None):
 
 def main():
     pass
+    
 
 if __name__ == "__main__":
     main()
